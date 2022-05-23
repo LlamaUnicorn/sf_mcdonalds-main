341177d4b72bd3c2a0d47b56224013c4e653e0b5
LlamaUnicorn <holidaynight@gmail.com>
2022-05-23 20:57:39 +0300

    Initial commit

---
+++ board/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import IndexView, NewOrderView, take_order
+
+urlpatterns = [
+    path('', IndexView.as_view()),
+    path('new/', NewOrderView.as_view(), name = 'new_order'),
+    path('take/<int:oid>', take_order, name = 'take_order')
+]
+
+# Example
+# urlpatterns = [
+#     path('', IndexView.as_view()),
+# ]
+# End of example
