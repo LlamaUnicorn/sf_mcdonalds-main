9acd2267ed411be64826bab51e89498ac6dd474b
LlamaUnicorn <holidaynight@gmail.com>
2022-05-24 20:23:08 +0300

    D7.5 counter to 5 (everything before crontab)

--- board/tasks.py
+++ board/tasks.py
@@ -3,23 +3,23 @@ from .models import Order
 import time
 
 
-@shared_task
-def complete_order(oid):
-    order = Order.objects.get(pk=oid)
-    order.complete = True
-    order.save()
+# @shared_task
+# def complete_order(oid):
+#     order = Order.objects.get(pk=oid)
+#     order.complete = True
+#     order.save()
 
 
 # Example
-# @shared_task
-# def hello():
-#     time.sleep(10)
-#     print("Hello, world!")
-#
-#
-# @shared_task
-# def printer(N):
-#     for i in range(N):
-#         time.sleep(1)
-#         print(i+1)
+@shared_task
+def hello():
+    time.sleep(10)
+    print("Hello, world!")
+
+
+@shared_task
+def printer(N):
+    for i in range(N):
+        time.sleep(1)
+        print(i+1)
 # End of example
--- mcdonalds/celery.py
+++ mcdonalds/celery.py
@@ -6,4 +6,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdonalds.settings')
 app = Celery('mcdonalds')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.beat_schedule = {
+    'print_every_5_seconds': {
+        'task': 'board.tasks.printer',
+        'schedule': 5,
+        'args': (5,),
+    },
+}
+
 app.autodiscover_tasks()
